b57c9f55e8b0849168f8ece9a48920f14ab77be9
先規　勇樹 <yukisaki80@gmail.com>
2021-03-06 22:41:47 +0900

    initial commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use NunoMaduro\Collision\Adapters\Phpunit\Style;
+
+
+class HelloController extends Controller
+{
+
+    public function index() 
+    {
+        return view('hello.index');
+    }
+
+public function post(Request $request)
+{
+    return view('hello.index', ['msg'=>$request->msg]);
+}
+}
---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {font-size:16pt; color:#999; }
+        h1 { font-size:50pt; text-align:right; color:#f6f6f6;
+        margin:-20px 0px -30px 0px; letter-spacing: -4pt; }
+    </style>
+</head>
+<body>
+    <h1>Blade/Index</h1>
+    @isset ($msg)
+    <p>こんにちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いてください。</p>
+    @endif
+    <form method="POST" action="/hello">
+        @csrf
+        <input type="text" name="msg">
+        <input type="submit">
+    </form>
+</body>
+</html>
